2daac63f10517512eafeb7cdf68d490a98786545
Timo Westkämper <timo.westkamper@nosto.com>
2025-01-29 15:50:21 +0200

    Infer result type from hit decorators

--- src/search.ts
+++ src/search.ts
@@ -1,11 +1,24 @@
 import { nostojs } from "@nosto/nosto-js"
 import { SearchOptions, SearchProduct, SearchQuery, SearchResult } from "@nosto/nosto-js/client"
 
-export type Options = SearchOptions & {
+export type Options<HD extends HitDecorator[]> = SearchOptions & {
   /**
    * Hit decorators to apply to the search results.
    */
-  hitDecorators?: HitDecorator[]
+  hitDecorators?: HD
+}
+
+export type HitDecorator = (hit: SearchProduct) => SearchProduct
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ToIntersection<U> = (U extends any ? (x: U) => void : never) extends (x: infer I) => void ? I : never
+
+type DecoratedProduct<HD extends HitDecorator[]> = ToIntersection<ReturnType<HD[number]>>
+
+export type DecoratedResult<HD extends HitDecorator[]> = Omit<SearchResult, "products"> & {
+  products: SearchResult["products"] & {
+    hits: DecoratedProduct<HD>[]
+  }
 }
 
 /**
@@ -15,25 +28,20 @@ export type Options = SearchOptions & {
  * @param options - An object containing optional parameters for the search.
  * @returns A promise that resolves to the search result.
  */
-export async function search(query: SearchQuery, { hitDecorators, ...options }: Options = {}): Promise<SearchResult> {
+export async function search<HD extends HitDecorator[]>(query: SearchQuery, options: Options<HD> = {}) {
+  const { hitDecorators, ...rest } = options
   const api = await new Promise(nostojs)
-
-  if (hitDecorators?.length) {
-    return applyDecorators(await api.search(query, options), hitDecorators)
-  }
-  return await api.search(query, options)
+  return applyDecorators(await api.search(query, rest), hitDecorators)
 }
 
-export type HitDecorator = (hit: SearchProduct) => SearchProduct
-
-function applyDecorators(response: SearchResult, decorators: HitDecorator[]) {
-  if (!response.products) {
-    return response
+function applyDecorators<HD extends HitDecorator[]>(response: SearchResult, decorators?: HD) {
+  if (!response.products || !decorators?.length) {
+    return response as DecoratedResult<HD>
   }
-  const decorator: HitDecorator = product => {
+  const decorator = (product: SearchProduct) => {
     return decorators.reduce((acc, decorator) => {
       return decorator(acc)
-    }, product)
+    }, product) as DecoratedProduct<HD>
   }
 
   return {
--- test/search.spec.ts
+++ test/search.spec.ts
@@ -39,6 +39,31 @@ describe("search", () => {
     ])
   })
 
+  it("should infer the type of the decorated results", async () => {
+    const decorator = (hit: SearchProduct) => ({
+      ...hit,
+      decorated: `${hit.name} Decorated`
+    })
+
+    const { products } = await search(query, { hitDecorators: [decorator] })
+    expect(products?.hits[0].decorated).toBe("Product 1 Decorated")
+  })
+
+  it("should infer the type of the decorated results from multiple decorators", async () => {
+    const decorator1 = (hit: SearchProduct) => ({
+      ...hit,
+      decorated1: `${hit.name} decorated 1`
+    })
+    const decorator2 = (hit: SearchProduct) => ({
+      ...hit,
+      decorated2: `${hit.name} decorated 2`
+    })
+
+    const { products } = await search(query, { hitDecorators: [decorator1, decorator2] })
+    expect(products?.hits[0].decorated1).toBe("Product 1 decorated 1")
+    expect(products?.hits[0].decorated2).toBe("Product 1 decorated 2")
+  })
+
   it("should handle empty search results", async () => {
     mockNostojs({
       search: vi.fn().mockResolvedValue({ products: { hits: [] } })
