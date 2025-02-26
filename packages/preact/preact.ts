38e9db918ba3a3e67311874f14309690785c385d
Maija Lintunokka <maija.lintunokka@nosto.com>
2025-02-26 16:26:35 +0200

    fix: preact exports

--- packages/preact/preact.ts
+++ packages/preact/preact.ts
@@ -1,2 +1,6 @@
 /** @module preact */
+export { useNostoState } from "./src/hooks/useNostoState"
 export { usePersonalization } from "./src/hooks/usePersonalization"
+export { type State, type Store } from "./src/store"
+export { StoreContext, StoreProvider } from "./src/storeContext"
+export { mockStore } from "./test/mocks/mocks"
