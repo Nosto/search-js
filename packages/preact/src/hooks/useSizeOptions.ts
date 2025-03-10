7dfe535e35093d4c7ea960e4d4f113e02c01a18c
Maija Lintunokka <maija.lintunokka@nosto.com>
2025-03-10 21:32:00 +0200

    chore: move useSizeOptions

---
+++ packages/preact/src/hooks/useSizeOptions.ts
@@ -0,0 +1,78 @@
+import { useActions } from "./useActions"
+import { useNostoAppState } from "./useNostoAppState"
+
+function normalize(value: number) {
+  return value && !Number.isNaN(value) ? value : 0
+}
+
+/**
+ * Preact hook that import size options to the component.
+ * @param sizes
+ * @param serpSize
+ * @example
+ * ```jsx
+ * import { useSizeOptions } from "@nosto/search-js/preact"
+ * import { defaultConfig, sizes } from "../config"
+ *
+ * export default () => {
+ *    const { from, to, size, total, handleSizeChange, sizeOptions } = useSizeOptions(sizes, defaultConfig.serpSize)
+ *
+ * return (
+ *  <div>
+ *    <div>
+ *      {from} - {total < to ? total : to} of {total} items
+ *    </div>
+ *     {sizeOptions.length > 0 && (
+ *      <div>
+ *        <select
+ *          value={size}
+ *          onChange={e => handleSizeChange(e.target.value)}
+ *        >
+ *          {sizeOptions.map(v => (
+ *            <option value={v}>{v} items per page</option>
+ *          ))}
+ *        </select>
+ *      </div>
+ *    )}
+ *  </div>
+ *  )
+ * }
+ * ```
+ * @group Hooks
+ */
+export function useSizeOptions(sizes: number[], serpSize: number) {
+  const { from, size, total } = useNostoAppState(state => ({
+    from: normalize(state.query.products?.from ?? 0),
+    size: normalize(state.response?.products?.size ?? 0),
+    total: normalize(state.response?.products?.total ?? 0)
+  }))
+
+  const { updateSearch } = useActions()
+
+  const to = from + size
+
+  const sizeOptions = [...sizes].reverse().filter(value => value + serpSize < total)
+
+  const handleSizeChange = (size: number) => {
+    updateSearch({
+      products: {
+        size
+      }
+    })
+  }
+
+  return {
+    /** from value */
+    from,
+    /** to value */
+    to,
+    /** total value */
+    total,
+    /** size value */
+    size,
+    /** Array of size options */
+    sizeOptions,
+    /** Should be called when size is changed */
+    handleSizeChange
+  }
+}
