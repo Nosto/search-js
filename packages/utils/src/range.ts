630135780bd7ff0ccdb6d8560e9d053c3738b96e
tomas.jaseliunas <tomas.jaseliunas@searchnode.com>
2025-03-27 09:28:39 +0200

    range comment

--- packages/utils/src/range.ts
+++ packages/utils/src/range.ts
@@ -1,3 +1,13 @@
+/**
+ * Generate an array of numbers from start to exlusive end
+ * @param start number of array range start
+ * @param end number of array range end
+ * @returns an array of numbers from start to exlusive end
+ * @example
+ * ```ts
+ * range(1, 5) // [1, 2, 3, 4]
+ * ```
+ */
 export function range(start: number, end: number): number[] {
   const diff = end - start
   if (!isNaN(diff) && diff > 0) {
