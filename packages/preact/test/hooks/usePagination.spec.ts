443e26d2f2fc2a317705e7f2dc02c2889cd62b54
tomas.jaseliunas <tomas.jaseliunas@searchnode.com>
2025-03-27 09:38:58 +0200

    typo

--- packages/preact/test/hooks/usePagination.spec.ts
+++ packages/preact/test/hooks/usePagination.spec.ts
@@ -22,7 +22,7 @@ describe("usePagination", () => {
     }
   })
 
-  function mockUsePegination(from: number, total: number, width = 10) {
+  function mockUsePagination(from: number, total: number, width = 10) {
     store.updateState({
       query: {
         products: {
@@ -42,19 +42,19 @@ describe("usePagination", () => {
   }
 
   it("previous for first page is undefined", () => {
-    const { prev, next } = mockUsePegination(0, 100)
+    const { prev, next } = mockUsePagination(0, 100)
     expect(prev).toBeUndefined()
     expect(next).toEqual({ current: false, from: 10, page: 2 })
   })
 
   it("next for last page is undefined", () => {
-    const { prev, next } = mockUsePegination(90, 100)
+    const { prev, next } = mockUsePagination(90, 100)
     expect(prev).toEqual({ current: false, from: 80, page: 9 })
     expect(next).toBeUndefined()
   })
 
   it("pages content is valid", () => {
-    const { pages } = mockUsePegination(0, 30)
+    const { pages } = mockUsePagination(0, 30)
     expect(pages).toEqual([
       { page: 1, from: 0, current: true },
       { page: 2, from: 10, current: false },
