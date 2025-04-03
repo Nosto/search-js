aa277b464013ca8fcd03eb4122accd0f3367721b
tomas.jaseliunas <tomas.jaseliunas@searchnode.com>
2025-04-03 16:16:28 +0300

    fix: added tests for infinite scroll

---
+++ packages/preact/test/components/InfiniteScroll/InfiniteScroll.spec.tsx
@@ -0,0 +1,75 @@
+// import { botUserAgent, mockUserAgent } from "./utils"
+// mockUserAgent(botUserAgent)
+import { InfiniteScroll } from "@preact/components/InfiniteScroll/InfiniteScroll"
+import { makeSerpConfig } from "@preact/config/pages/serp/config"
+import { SearchPageProvider } from "@preact/config/pages/serp/provider"
+import { render } from "@testing-library/preact"
+import * as utils from "@utils/isBot"
+import { describe, expect, it, vi } from "vitest"
+
+import { mockStore } from "../../mocks/mocks"
+
+describe("InfiniteScroll", () => {
+  vi.spyOn(utils, "isBot").mockReturnValue(true)
+
+  const store = mockStore({
+    loading: false,
+    initialized: true,
+    query: {
+      products: {
+        from: 0
+      }
+    },
+    response: {
+      products: {
+        size: 10,
+        total: 100,
+        hits: []
+      }
+    }
+  })
+
+  const InfiniteScrollComponent = () => {
+    return (
+      <SearchPageProvider config={makeSerpConfig({})} store={store}>
+        <InfiniteScroll pageSize={10}>
+          <div>Child 1</div>
+          <div>Child 2</div>
+        </InfiniteScroll>
+      </SearchPageProvider>
+    )
+  }
+
+  it("renders children", () => {
+    const result = render(<InfiniteScrollComponent />)
+
+    expect(result.getByText("Child 1")).toBeDefined()
+    expect(result.getByText("Child 2")).toBeDefined()
+  })
+
+  it("renders load more button when there are more results", () => {
+    const result = render(<InfiniteScrollComponent />)
+
+    expect(result.getByText("More results")).toBeDefined()
+  })
+
+  it("does not render load more button when there are no more results", () => {
+    store.updateState({
+      query: {
+        products: {
+          from: 100
+        }
+      },
+      response: {
+        products: {
+          size: 10,
+          total: 100,
+          hits: []
+        }
+      }
+    })
+
+    const result = render(<InfiniteScrollComponent />)
+    expect(result.queryByText("More results")).toBeNull()
+  })
+})
