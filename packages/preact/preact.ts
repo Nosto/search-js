49fde59b00aa0849e7afb5cb003c13d35835664a
tomas.jaseliunas <tomas.jaseliunas@searchnode.com>
2025-04-03 16:45:18 +0300

    isBot import fixed

--- packages/preact/preact.ts
+++ packages/preact/preact.ts
@@ -1,5 +1,4 @@
 /** @module preact */
-export { isBot } from "../utils/src/isBot"
 export { InfiniteScroll, type InfiniteScrollProps } from "./src/components/InfiniteScroll/InfiniteScroll"
 export { InfiniteScrollWithLink } from "./src/components/InfiniteScroll/InfiniteScrollWithLink"
 export { SearchInput } from "./src/components/SearchInput"
@@ -29,3 +28,4 @@ export { useSort } from "./src/hooks/useSort/useSort"
 export { createStore, defaultState, type State, type Store } from "./src/store"
 export { StoreContext } from "./src/storeContext"
 export { createExtendableStore, type ExtendedStore } from "./src/storeExtensions"
+export { isBot } from "@utils/isBot"
