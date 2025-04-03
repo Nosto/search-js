fa69aa9d68a05fda98f7d9c2faccad228267e422
tomas.jaseliunas <tomas.jaseliunas@searchnode.com>
2025-04-03 14:03:48 +0300

    fix: additional component exports

--- packages/preact/src/components/InfiniteScroll/InfiniteScrollWithLink.tsx
+++ packages/preact/src/components/InfiniteScroll/InfiniteScrollWithLink.tsx
@@ -7,6 +7,27 @@ import { hasMoreResults } from "./utils"
 
 /**
  * Infinite scroll component that loads more results when user clicks a link.
+ * * @example
+ * ```tsx
+ * import { InfiniteScrollWithLink } from "@nosto/search-js/preact"
+ *
+ * // With custom load more button
+ * function LoadMoreButton({ pageSize }) {
+ *     const { loadMore } = useLoadMore(pageSize)
+ *
+ *     return (
+ *         <button
+ *              onClick={loadMore}
+ *          >
+ *              More results
+ *         </button>
+ *    )
+ * }
+ *
+ * <InfiniteScrollWithLink loadMoreComponent={LoadMoreButton} pageSize={defaultConfig.pageSize}>
+ *   {products?.hits?.map(hit => <Product hit={hit} />)}
+ * </InfiniteScrollWithLink>
+ * ```
  * @group Components
  */
 export function InfiniteScrollWithLink({ children, loadMoreComponent: LoadMore, pageSize }: InfiniteScrollProps) {
