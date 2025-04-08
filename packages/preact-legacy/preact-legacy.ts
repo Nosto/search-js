fa10276e00e1729e867d49a9758cf8e6d5d92adc
tomas.jaseliunas <tomas.jaseliunas@searchnode.com>
2025-04-08 11:20:44 +0300

    docs update

--- packages/preact-legacy/preact-legacy.ts
+++ packages/preact-legacy/preact-legacy.ts
@@ -1,3 +1,4 @@
+/** @module preact-legacy */
 export { InfiniteScrollWithObserver } from "@preact/components/InfiniteScroll/InfiniteScrollWithObserver"
 export { intersectionObserverSupported } from "@preact/components/InfiniteScroll/utils"
 export { getNextPageQuery } from "@preact/hooks/useLoadMore/getNextPageQuery"
