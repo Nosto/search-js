05c4949dbdbd97c13619ca174fe7460b4a3d6ab2
tomas.jaseliunas <tomas.jaseliunas@searchnode.com>
2025-04-08 10:50:01 +0300

    fix: preact legacy exports

---
+++ packages/preact-legacy/preact-legacy.ts
@@ -0,0 +1,3 @@
+export { InfiniteScrollWithObserver } from "@preact/components/InfiniteScroll/InfiniteScrollWithObserver"
+export { intersectionObserverSupported } from "@preact/components/InfiniteScroll/utils"
+export { getNextPageQuery } from "@preact/hooks/useLoadMore/getNextPageQuery"
