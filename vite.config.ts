ca2df1dc430658188e9b7fa5b63721ec35f9b9ef
tomas.jaseliunas <tomas.jaseliunas@searchnode.com>
2025-04-15 11:57:48 +0300

    fix: organise exports

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { resolve } from "path"
 import dts from "vite-plugin-dts"
 import { defineConfig } from "vitest/config"
 
-const packages = ["core", "currencies", "preact", "thumbnails", "utils", "preact-legacy"]
+const basePackages = ["core", "currencies", "preact", "thumbnails", "utils", "preact-legacy"]
+const preactPackages = ["common", "serp", "autocomplete", "category", "hooks"]
+const packages = [...basePackages, ...preactPackages]
 
 export default defineConfig({
   plugins: [
@@ -13,7 +15,9 @@ export default defineConfig({
         noEmit: false,
         emitDeclarationOnly: true
       },
-      exclude: ["dist", "**/vite.config.ts", "**/test", "dev"]
+      include: ["packages/**/*", ...preactPackages.map(name => `packages/preact/${name}.ts`)],
+      exclude: ["dist", "**/vite.config.ts", "**/test", "dev"],
+      outDir: "dist"
     })
   ],
   build: {
@@ -21,8 +25,16 @@ export default defineConfig({
     lib: {
       name: "@nosto/search-js",
       formats: ["es", "cjs"],
-      entry: packages.map(name => resolve(import.meta.dirname, `packages/${name}/${name}.ts`)),
-      fileName: (format, filename) => `${filename}/${filename}.${format}.js`
+      entry: packages.flatMap(name => {
+        return preactPackages.includes(name)
+          ? resolve(import.meta.dirname, `packages/preact/${name}.ts`)
+          : resolve(import.meta.dirname, `packages/${name}/${name}.ts`)
+      }),
+      fileName: (format, filename) => {
+        return preactPackages.includes(filename)
+          ? `preact/${filename}.${format}.js`
+          : `${filename}/${filename}.${format}.js`
+      }
     },
     rollupOptions: {
       external: ["preact", "preact/hooks"]
@@ -35,7 +47,8 @@ export default defineConfig({
       "@preact": resolve(import.meta.dirname, "packages/preact/src"),
       "@thumbnails": resolve(import.meta.dirname, "packages/thumbnails/src"),
       "@utils": resolve(import.meta.dirname, "packages/utils/src"),
-      "@preact-legacy": resolve(import.meta.dirname, "packages/preact-legacy/src")
+      "@preact-legacy": resolve(import.meta.dirname, "packages/preact-legacy/src"),
+      "@common": resolve(import.meta.dirname, "packages/preact/common")
     }
   },
   test: {
