3ccd0d914d1fafffe509e1df972a33b71beb5d84
tomas.jaseliunas <tomas.jaseliunas@searchnode.com>
2025-04-16 15:39:05 +0300

    chore: legacy move

---
+++ packages/preact/legacy.ts
@@ -0,0 +1,4 @@
+/** @module preact/legacy */
+export { InfiniteScrollWithObserver } from "./src/components/InfiniteScroll/InfiniteScrollWithObserver"
+export { intersectionObserverSupported } from "./src/components/InfiniteScroll/utils"
+export { getNextPageQuery } from "./src/hooks/useLoadMore/getNextPageQuery"
