a73ebd3738b131d028953e423f0d3f126935d093
aistetamos <aiste@searchnode.com>
2025-05-09 15:39:05 +0300

    chore: add component for testing

---
+++ dev/preact/src/pages/Search/ProductSwatches.tsx
@@ -0,0 +1,39 @@
+import { SearchProduct } from "@nosto/nosto-js/client"
+import { useSwatches } from "@nosto/search-js/preact/hooks"
+
+export default function ProductSwatches({ skus = [] }: SearchProduct) {
+  const { swatches, toggleOption } = useSwatches(skus, ["color", "size", "material"])
+
+  if (!swatches || swatches.length === 0) {
+    return
+  }
+
+  return (
+    <div>
+      {swatches.map(({ field, options }) => (
+        <div key={field}>
+          <div>
+            {options.map(({ value, unavailable, selected }) => (
+              <button
+                key={value}
+                onClick={e => {
+                  e.preventDefault()
+                  toggleOption(field, value)
+                }}
+                disabled={unavailable}
+                style={{
+                  margin: "5px",
+                  background: selected ? "#0073e6" : "#f5f5f5",
+                  color: selected ? "#ffffff" : "#000000",
+                  opacity: unavailable ? 0.2 : 1
+                }}
+              >
+                {value}
+              </button>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
