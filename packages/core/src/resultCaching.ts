c37eed241891af70cd660af6ee71e403398377c9
Timo Westkämper <timo.westkamper@nosto.com>
2025-05-18 17:42:34 +0300

    feat: use CacheStorage API

--- packages/core/src/resultCaching.ts
+++ packages/core/src/resultCaching.ts
@@ -1,41 +1,51 @@
 import { SearchQuery, SearchResult } from "@nosto/nosto-js/client"
-import { isEqual } from "@utils/isEqual"
-import { getSessionStorageItem, setSessionStorageItem } from "@utils/storage"
 
-export const STORAGE_ENTRY_NAME = "nosto:search-js:cache"
+const CACHE_NAME = "nosto-search-cache"
 
 const TTL = 60 * 1000 // 1 minute
+const MAX_CACHE_SIZE = 20
 
 type CacheEntry = {
-  query: SearchQuery
   result: SearchResult
   created: number
 }
 
-export function cacheSearchResult(query: SearchQuery, result: SearchResult) {
-  setSessionStorageItem(STORAGE_ENTRY_NAME, { query, result, created: Date.now() })
+function getRequestForQuery(query: SearchQuery) {
+  const cacheKey = getCacheKey(query)
+  return new Request(`http://${CACHE_NAME}/${encodeURIComponent(cacheKey)}`)
 }
 
-export function loadCachedResult(query: SearchQuery) {
-  const storageValue = getSessionStorageItem<CacheEntry>(STORAGE_ENTRY_NAME)
-  if (!storageValue || !isValueShapeCorrect(storageValue)) {
-    return null
+export async function cacheSearchResult(query: SearchQuery, result: SearchResult) {
+  const cache = await caches.open(CACHE_NAME)
+  const requests = await cache.keys()
+  if (requests.length > MAX_CACHE_SIZE) {
+    // Remove the oldest entry (FIFO)
+    await cache.delete(requests[0])
   }
+  const request = getRequestForQuery(query)
+  const response = new Response(JSON.stringify({ result, created: Date.now() }), {
+    headers: { "Content-Type": "application/json" }
+  })
+  await cache.put(request, response)
+}
 
-  const cachedQuery = getCacheKey(storageValue.query)
-  if (!isEqual(getCacheKey(query), cachedQuery)) {
+export async function loadCachedResult(query: SearchQuery) {
+  const cache = await caches.open(CACHE_NAME)
+  const request = getRequestForQuery(query)
+  const response = await cache.match(request)
+  if (!response) {
     return null
   }
+  const storageValue = (await response.json()) as CacheEntry
   if (Date.now() - storageValue.created > TTL) {
     return null
   }
-
   return storageValue.result
 }
 
 // This function is used to create a cache key for the search query.
 // It removes the time and size properties from the query object.
-function getCacheKey(query: SearchQuery): SearchQuery {
+function getCacheKey(query: SearchQuery) {
   const params = {
     ...query,
     time: undefined,
@@ -44,11 +54,5 @@ function getCacheKey(query: SearchQuery): SearchQuery {
       size: undefined
     }
   }
-
-  return JSON.parse(JSON.stringify(params))
-}
-
-// TODO: Better validation with valibot
-function isValueShapeCorrect(value: unknown): value is CacheEntry {
-  return typeof value === "object" && value !== null && "query" in value && "result" in value && "created" in value
+  return JSON.stringify(params)
 }
