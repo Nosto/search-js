e4f6481550da3dd5931a02a8fa8dcc9918d0978d
tomas.jaseliunas <tomas.jaseliunas@searchnode.com>
2025-06-27 13:46:28 +0300

    fix: memo for observer infinite scroll

--- packages/preact/src/components/InfiniteScroll/InfiniteScrollWithObserver.tsx
+++ packages/preact/src/components/InfiniteScroll/InfiniteScrollWithObserver.tsx
@@ -1,7 +1,9 @@
 import { useLoadMore } from "@preact/hooks/useLoadMore/useLoadMore"
 import { useNostoAppState } from "@preact/hooks/useNostoAppState"
+import { isEqual } from "@utils/isEqual"
 import { pick } from "@utils/pick"
 import { JSX } from "preact"
+import { memo } from "preact/compat"
 import { useEffect, useRef } from "preact/hooks"
 
 import { type InfiniteScrollProps } from "./InfiniteScroll"
@@ -11,47 +13,46 @@ import { hasMoreResults } from "./utils"
  * Infinite scroll component that loads more results when user scrolls to the end of the page.
  * @group Components
  */
-export function InfiniteScrollWithObserver({
-  children,
-  pageSize,
-  observerOptions = {}
-}: InfiniteScrollProps): JSX.Element {
-  const endResultsRef = useRef<HTMLDivElement>(null)
-  const { query, response } = useNostoAppState(state => pick(state, "query", "response"))
+export const InfiniteScrollWithObserver = memo(
+  ({ children, pageSize, observerOptions }: InfiniteScrollProps): JSX.Element => {
+    const endResultsRef = useRef<HTMLDivElement>(null)
+    const { query, response } = useNostoAppState(state => pick(state, "query", "response"))
 
-  const { loadMore } = useLoadMore(pageSize)
+    const { loadMore } = useLoadMore(pageSize)
 
-  useEffect(() => {
-    let loader: HTMLDivElement | null
-    let observer: IntersectionObserver
+    useEffect(() => {
+      let loader: HTMLDivElement | null
+      let observer: IntersectionObserver
 
-    if (hasMoreResults(query, response)) {
-      loader = endResultsRef.current
-      observer = new IntersectionObserver(entries => {
-        const target = entries[0]
-        if (target?.isIntersecting) {
-          loadMore()
+      if (hasMoreResults(query, response)) {
+        loader = endResultsRef.current
+        observer = new IntersectionObserver(entries => {
+          const target = entries[0]
+          if (target?.isIntersecting) {
+            loadMore()
+          }
+        }, observerOptions)
+
+        if (loader) {
+          observer.observe(loader)
         }
-      }, observerOptions)
-
-      if (loader) {
-        observer.observe(loader)
       }
-    }
 
-    return () => {
-      if (loader) {
-        observer.unobserve(loader)
+      return () => {
+        if (loader) {
+          observer.unobserve(loader)
+        }
       }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [response])
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [response])
 
-  return (
-    <>
-      {children}
-      {/* Inlining height style for Safari support, so the IntersectionObserver sees the element. */}
-      <div ref={endResultsRef} style={{ height: "1px" }} />
-    </>
-  )
-}
+    return (
+      <>
+        {children}
+        {/* Inlining height style for Safari support, so the IntersectionObserver sees the element. */}
+        <div ref={endResultsRef} style={{ height: "1px" }} />
+      </>
+    )
+  },
+  isEqual
+)
