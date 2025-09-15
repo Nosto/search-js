e24b00cd81e6af8a133acd781106d433a7a3dc63
Timo Westkämper <timo.westkamper@nosto.com>
2025-09-15 12:26:31 +0300

    fix: expose config hooks

--- packages/preact/common/common.ts
+++ packages/preact/common/common.ts
@@ -1,6 +1,7 @@
 /** @module preact/common */
 export { InfiniteScroll, type InfiniteScrollProps } from "./src/components/InfiniteScroll/InfiniteScroll"
 export { InfiniteScrollWithLink } from "./src/components/InfiniteScroll/InfiniteScrollWithLink"
+export { useAutocompleteConfig, useCategoryConfig, useConfig, useSerpConfig } from "./src/config/configContext"
 export { createStore, defaultState, type State, type Store } from "./src/store/store"
 export { StoreContext } from "./src/store/storeContext"
 export { createExtendableStore, type ExtendedStore } from "./src/store/storeExtensions"
