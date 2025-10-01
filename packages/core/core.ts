540e522d2f1adbd58e2f9c75fb0ea2a5c7219cac
Timo Westkämper <timo.westkamper@nosto.com>
2025-10-01 14:44:04 +0300

    fix: use core prefix instead of ./

--- packages/core/core.ts
+++ packages/core/core.ts
@@ -1,4 +1,4 @@
-/** @module ./ */
+/** @module core */
 export { addToCart, type SearchHitWithSku } from "./src/addToCart"
 export { search } from "./src/search"
 export type { DecoratedProduct, DecoratedResult, HitDecorator, SearchOptions, ToIntersection } from "./src/types"
