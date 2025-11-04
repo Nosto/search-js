b537f79eca0d1e6337afba1e999ffd0f96561a0a
Timo Westkämper <timo.westkamper@nosto.com>
2025-11-04 14:08:19 +0200

    feat: add searchWithRedirects

---
+++ packages/core/src/withRedirects.ts
@@ -0,0 +1,21 @@
+import { SearchOptions, SearchQuery, SearchResult } from "@nosto/nosto-js/client"
+
+import { SearchFn } from "./types"
+
+export async function searchWithRedirects(
+  query: SearchQuery,
+  { redirect, ...options }: SearchOptions,
+  searchFn: SearchFn
+): Promise<SearchResult> {
+  if (!redirect) {
+    return searchFn(query, options)
+  }
+  const response = await searchFn(query, options)
+  if (response.keywords?.hits?.length) {
+    const match = response.keywords.hits.find(hit => hit.keyword === query.query && hit._redirect)
+    if (match && match._redirect) {
+      window.location.href = match._redirect
+    }
+  }
+  return response
+}
---
+++ packages/core/test/searchWithRedirects.spec.ts
@@ -0,0 +1,50 @@
+import { searchWithRedirects } from "@core/withRedirects"
+import { SearchQuery } from "@nosto/nosto-js/client"
+import { beforeEach, describe, expect, it, vi } from "vitest"
+
+describe("searchWithRedirects", () => {
+  const search = vi.fn()
+  const query: SearchQuery = { query: "shoes" }
+
+  beforeEach(() => {
+    vi.resetAllMocks()
+  })
+
+  it("should return response directly if redirect is false", async () => {
+    const result = {
+      products: {
+        hits: [{ id: "1", name: "product 1" }],
+        total: 1,
+        size: 1
+      }
+    }
+    search.mockResolvedValue(result)
+
+    const response = await searchWithRedirects(query, { redirect: false }, search)
+    expect(response).toEqual(result)
+    expect(search).toHaveBeenCalledTimes(1)
+  })
+
+  it("should redirect if a matching keyword with _redirect is found", async () => {
+    const redirectUrl = "https://example.com/redirect"
+    const result = {
+      keywords: {
+        hits: [{ keyword: "shoes", _redirect: redirectUrl }, { keyword: "hats" }],
+        total: 2,
+        size: 2
+      }
+    }
+    search.mockResolvedValue(result)
+
+    // Mock window.location.href
+    vi.stubGlobal("window", {
+      location: {
+        href: ""
+      }
+    })
+
+    await searchWithRedirects(query, { redirect: true }, search)
+    expect(window.location.href).toBe(redirectUrl)
+    expect(search).toHaveBeenCalledTimes(1)
+  })
+})
